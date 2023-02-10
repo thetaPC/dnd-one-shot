ecfcf365596f152ddb5ec0fb40ed75eb1a020de1
Maria Hutt <thetaPC@users.noreply.github.com>
2023-02-09 17:08:35 -0800

    chore(20136620): Implement ChakraUI. (#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { ChakraProvider } from '@chakra-ui/react';
+
 import './globals.css'
 
 export default function RootLayout({
@@ -12,7 +16,11 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <ChakraProvider>
+          {children}
+        </ChakraProvider>
+      </body>
     </html>
   )
 }
